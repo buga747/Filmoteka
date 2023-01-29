f87b96ecc8a6de0bc8f99eaff43e434afe297dc7
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-30 01:11:34 +0200

    quick fix

--- src/js/galleryFilmsMarkup.js
+++ src/js/galleryFilmsMarkup.js
@@ -7,8 +7,16 @@ export function renderFilmsGallery(arr) {
   const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 
   const markup = arr
-    .map(({ poster_path, title, genre_ids: genreIds, id }) => {
-      return `<div class="film-wrapper movie__item" data-id='${id}'>
+    .map(
+      ({
+        poster_path,
+        title,
+        genre_ids: genreIds,
+        id,
+        release_date,
+        vote_average,
+      }) => {
+        return `<div class="film-wrapper movie__item" data-id='${id}'>
                 <img class="film-img" 
                 src="${poster_path ? POSTER_URL + poster_path : ComingSoonImg}" 
                 alt="Movie ${title ? title : 'Unknown title'} poster>
@@ -19,10 +27,13 @@ export function renderFilmsGallery(arr) {
                     <p class="film-genres">${getGenresNames(
                       genreIds,
                       genresInfo
-                    )}</p>
+                    )} | ${release_date.slice(0, 4)}
+        <span class="movie_rate">${vote_average.toFixed(1)}</span>            
+        </p>
                 </div>
             </div>`;
-    })
+      }
+    )
     .join('');
 
   fimGallery.innerHTML = markup;
