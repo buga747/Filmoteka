4974f1c4ff50bf5c0fd2475a80f966483c3bfb03
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-30 23:00:53 +0200

    bugs fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/index.scss';
 import './js/fetchApi';
 import './js/genres-info';
-import './js/firstLoadGalleryMarkup';
+import './js/onFirstLoadGalleryRendering';
 import './js/galleryFilmsMarkup';
 import './js/input-film-search';
 import './js/input-list-markup';
@@ -9,4 +9,3 @@ import './js/submit-film-search';
 import './js/renderMovieCard';
 import './js/loader';
 import './js/getGenreNames';
-import './js/pagination';
